b67fb18f3e44cd764a2a56101809c4d5f601c6ac
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-23 21:01:53 +0100

    [ADD] structure of import file

---
+++ src/import_export/import.c
@@ -0,0 +1,22 @@
+#include <err.h>
+#include "SDL2/SDL_image.h"
+
+void import_jpg(char* file_path)
+{
+	//TODO
+}
+
+void import_png(char* file_path)
+{
+	//TODO
+}
+
+void import_tiff(char* file_path)
+{
+	//TODO
+}
+
+void import_bmp(char* file_path)
+{
+	//TODO
+}
---
+++ src/import_export/import.h
@@ -0,0 +1,12 @@
+#ifndef IMPORT_H
+#define IMPORT_H
+
+void import_jpg(char* file_path);
+
+void import_png(char* file_path);
+
+void import_tiff(char* file_path);
+
+void import_bmp(char* file_path);
+
+#endif
