f90914c88f15c91919503d81afd21fe51dfd7c4d
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-26 19:51:35 +0100

    [ADD] module to decrease the saturation of an image

--- src/modules/saturation.c
+++ src/modules/saturation.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
 #include <err.h>
+#include "../imagin.h"
+#include "saturation.h"
+
+//For now, the saturation module doesn't handle increase saturation.
+void saturation(Image *img, double change)
+{
+	size_t i;
+	double P;
+
+	if (img)
+	{
+		for (i = 0; i < img->x * img->y; i++)
+		{
+			P = sqrt(img->data[i].red * img->data[i].red * 0.299 +
+			img->data[i].green * img->data[i].green * 0.587 +
+			img->data[i].blue * img->data[i].blue * 0.144);
+
+			img->data[i].red = P + ((img->data[i].red) - P) * change;
+			img->data[i].green= P + ((img->data[i].green) - P) * change;
+			img->data[i].blue = P + ((img->data[i].blue) - P) * change;
+		}
+	}
+}
--- src/modules/saturation.h
+++ src/modules/saturation.h
@@ -1,4 +1,6 @@
 #ifndef SATURATION_H
 #define SATURATION_H
 
+void saturation(Image *img, double change);
+
 #endif
