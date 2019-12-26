a56ee99bc54b1d3ac0591cd6da9daf25616560a6
alexis boissiere <alexis.boissiere@epita.fr>
2019-12-26 22:13:04 +0100

    [ADD] empty function for rotation

--- src/modules/rotate.c
+++ src/modules/rotate.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 #include <err.h>
+
+void rotate_left(Image *img)
+{
+	//TODO
+}
+
+
+void rotate_right(Image *img)
+{
+	//TODO
+}
--- src/modules/rotate.h
+++ src/modules/rotate.h
@@ -1,4 +1,8 @@
 #ifndef ROTATE_H
 #define ROTATE_H
 
+void rotate_left(Image *img);
+
+void rotate_right(Image *img);
+
 #endif
