8226b65dd8882ec51d71e18844830d2d8328e8da
Turodoras <turodoras@gmail.com>
2020-01-08 15:00:31 +0100

    [CHANGE] maxvalue to threshold in exposure.c

--- src/modules/exposure.c
+++ src/modules/exposure.c
@@ -8,26 +8,26 @@ void exposure(struct Image *img)
 {
     if (img)
     {
-        size_t maxvalue = pow(2, img->bit_depth-1);
+        size_t threshold = pow(2, img->bit_depth) - 1;
 
         for (size_t i=0; i < img->width * img->height; i++)
         {
             img->data[i].red *= 2;
-            if (img->data[i].red > maxvalue) //pixel value too big
+            if (img->data[i].red > threshold) //pixel value too big
             {
-                img->data[i].red = maxvalue;
+                img->data[i].red = threshold;
             }
 
             img->data[i].green *= 2;
-            if (img->data[i].green > maxvalue) //pixel value too big
+            if (img->data[i].green > threshold) //pixel value too big
             {
-                img->data[i].green = maxvalue;
+                img->data[i].green = threshold;
             }
 
             img->data[i].blue *= 2;
-            if (img->data[i].blue > maxvalue) //pixel value to big
+            if (img->data[i].blue > threshold) //pixel value to big
             {
-                img->data[i].blue = maxvalue;
+                img->data[i].blue = threshold;
             }
         }
     }
