81312ba7f37a7a4ed5ed113b0facedbc6ad6d8ce
Turodoras <turodoras@gmail.com>
2020-01-08 16:17:27 +0100

    [EXPOSURE] [FIX] bug with pow value

--- src/modules/exposure.c
+++ src/modules/exposure.c
@@ -8,27 +8,27 @@ void exposure(struct Image *img)
 {
     if (img)
     {
-        size_t threshold = pow(2, img->bit_depth) - 1;
-
-        for (size_t i=0; i < img->width * img->height; i++)
+        for (size_t i = 0; i < img->width * img->height; i++)
         {
-            img->data[i].red *= 2;
-            if (img->data[i].red > threshold) //pixel value too big
+            img->data[i].red += 200;
+            if (img->data[i].red > img->bit_depth) //pixel value too big
             {
-                img->data[i].red = threshold;
+                img->data[i].red = img->bit_depth;
             }
 
-            img->data[i].green *= 2;
-            if (img->data[i].green > threshold) //pixel value too big
+            img->data[i].green += 200;
+            if (img->data[i].green > img->bit_depth) //pixel value too big
             {
-                img->data[i].green = threshold;
+                img->data[i].green = img->bit_depth;
             }
 
-            img->data[i].blue *= 2;
-            if (img->data[i].blue > threshold) //pixel value to big
+            img->data[i].blue += 255;
+            if (img->data[i].blue > img->bit_depth) //pixel value to big
             {
-                img->data[i].blue = threshold;
+                img->data[i].blue = img->bit_depth;
             }
+
+            printf("red: %d, green: %d, blue: %d\n", img->data[i].red, img->data[i].green, img->data[i].blue);
         }
     }
 }
