232e17cd7aaf2d232647ca707b8c4eb5a2ba2839
Turodoras <turodoras@gmail.com>
2020-01-09 12:01:49 +0100

    [EXPOSURE] [WIP]

--- src/modules/exposure.c
+++ src/modules/exposure.c
@@ -10,25 +10,22 @@ void exposure(struct Image *img)
     {
         for (size_t i = 0; i < img->width * img->height; i++)
         {
-            img->data[i].red += 200;
-            if (img->data[i].red > img->bit_depth) //pixel value too big
-            {
-                img->data[i].red = img->bit_depth;
-            }
+            img->data[i].red = (img->data[i].red * 2) > img->bit_depth ?
+                img->bit_depth : 0;
+            //img->data[i].red = (img->data[i].red + 200) > img->bit_depth ?
+            //    img->bit_depth : img->data[i].red *= 2;
 
-            img->data[i].green += 200;
-            if (img->data[i].green > img->bit_depth) //pixel value too big
-            {
-                img->data[i].green = img->bit_depth;
-            }
+            img->data[i].green = (img->data[i].green * 2) > img->bit_depth ?
+                img->bit_depth : 0;
+            //img->data[i].green = (img->data[i].green + 200) > img->bit_depth ?
+            //    img->bit_depth : img->data[i].green *= 2;
 
-            img->data[i].blue += 255;
-            if (img->data[i].blue > img->bit_depth) //pixel value to big
-            {
-                img->data[i].blue = img->bit_depth;
-            }
+            img->data[i].blue = (img->data[i].blue * 2) > img->bit_depth ?
+                img->bit_depth : 0;
+            //img->data[i].blue = (img->data[i].blue + 200) > img->bit_depth ?
+            //    img->bit_depth : img->data[i].blue *= 2;
 
-            printf("red: %d, green: %d, blue: %d\n", img->data[i].red, img->data[i].green, img->data[i].blue);
+           printf("red: %d, green: %d, blue: %d\n", img->data[i].red, img->data[i].green, img->data[i].blue);
         }
     }
 }
