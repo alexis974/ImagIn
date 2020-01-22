732490acbfb261071c70eea344299ec74aaf643b
Alexandre <alexandre.fresnais@epita.fr>
2020-01-22 09:11:27 +0100

    [CLI][FIX] warnings and errors to make cli compile

--- src/imagin_cli.c
+++ src/imagin_cli.c
@@ -10,6 +10,7 @@
 #include "import_export/export.h"
 #include "import_export/xml.h"
 #include "import_export/scale.h"
+#include "import_export/free.h"
 
 #include "modules/black_and_white.h"
 #include "modules/invert.h"
@@ -48,18 +49,18 @@ int main(void)
     struct Images *images;
     images = malloc(sizeof(struct Images));
 
-    // Store all the pixel of the image in Full_image
-    images->full = read_image(img_path);
-    write_image("tmp/Full_image.jpg", images->full);
-    write_image("tmp/Full_image.ppm", images->full);
-    write_image("tmp/Full_image.tiff", images->full);
+    // Store all the pixel of the image
+    images = read_image(img_path);
+    write_image("tmp/Full_image.jpg", images);
+    write_image("tmp/Full_image.ppm", images);
+    write_image("tmp/Full_image.tiff", images);
 
     // Scale the image to fit gui window
     images->scale = scale_img(images->full, 1200, 800);
     images->edit = scale_img(images->full, 1200, 800);
     images->small = scale_img(images->full, 225, 150);
-    write_image("tmp/Scale_image.ppm", images->scale);
-    write_image("tmp/Small_image.ppm", images->small);
+    write_image("tmp/Scale_image.ppm", images);
+    write_image("tmp/Small_image.ppm", images);
 
 
 //##############################################################################
@@ -68,34 +69,29 @@ int main(void)
 
     //Add 0.5EV to image
     exposure(images->edit, 0.5);
-    write_image("tmp/01_Exposure.jpg", images->edit);
+    write_image("tmp/01_Exposure.jpg", images);
 
     //Invert the colors of an image
     invert(images->edit);
-    write_image("tmp/02_Invert.jpg", images->edit);
+    write_image("tmp/02_Invert.jpg", images);
 
     //Turn the image black and white
     simple_BW(images->edit);
-    write_image("tmp/03_Black_and_white.ppm", images->edit);
+    write_image("tmp/03_Black_and_white.ppm", images);
 
     //Flip the image both horizontaly and verticaly
     flip_both_axis(images->edit);
-    write_image("tmp/04_Flip_both_axis.tiff", images->edit);
+    write_image("tmp/04_Flip_both_axis.tiff", images);
 
     //Flip the horizontaly
     horizontal_flip(images->edit);
-    write_image("tmp/05_Flip_horizontal.jpg", images->edit);
+    write_image("tmp/05_Flip_horizontal.jpg", images);
 
     //Flip the image verticaly
     vertical_flip(images->edit);
-    write_image("tmp/06_Flip_vertiacl.ppm", images->edit);
+    write_image("tmp/06_Flip_vertiacl.ppm", images);
 
-    // Free image
-    free(images->full);
-    free(images->scale);
-    free(images->edit);
-    free(images->small);
-    free(images);
+    free_images(images);
 
     printf("See you soon!\n");
 
