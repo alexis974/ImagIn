97bcd8b3ba41dc6c922afe9d983baf57e27ebd99
Alexis Boyadjian <alexis.boyadjian@epita.fr>
2020-01-24 02:18:45 +0100

    [XML][ADD] file xml_writer and test_to_modify

---
+++ src/import_export/xml/xml_writer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <libxml/encoding.h>
+#include <libxml/xmlwriter.h>
+
+int main(void)
+{return 0;}
+
+//add_module(char attributes[], int status[], char mod_name[])
+//{}
