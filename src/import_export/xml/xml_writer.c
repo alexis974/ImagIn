2e346e894d82c4a8d7eafb824abdc6fd8f575adf
Alexis Boyadjian <alexis.boyadjian@epita.fr>
2020-01-24 03:11:34 +0100

    [XML][ADD] Parser for xml files in xml_writer

--- src/import_export/xml/xml_writer.c
+++ src/import_export/xml/xml_writer.c
@@ -2,9 +2,35 @@
 #include <string.h>
 #include <libxml/encoding.h>
 #include <libxml/xmlwriter.h>
+#include <xml_writer.h>
+#include <libxml/parser.h>
 
-int main(void)
-{return 0;}
+void add_module(char **attributes, int *status, char *mod_name)
+{
+   int rc;
+   xmlDoc         *document;
+   xmlNode        *root, *first_child, *node;
+}
 
-//add_module(char attributes[], int status[], char mod_name[])
-//{}
+void parser_xml(char *filename)
+{
+    xmlDoc         *document;
+    xmlNode        *root, *first_child, *node;
+
+    document = xmlReadFile(filename, NULL, 0);
+
+    root = xmlDocGetRootElement(document);
+
+    printf("Root is <%s> (%i)\n", root->name, root->type);
+
+    first_child = root->children;
+
+    for (node = first_child; node; node = node->next)
+    {
+        printf("\t Child is <%s> (%i)\n", node->name, node->type);
+    }
+
+    printf("...\n");
+
+    xmlFreeDoc(document);
+}
