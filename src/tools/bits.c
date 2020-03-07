53d97594ce558abd81dbf3328d4401af907aa765
Alexandre <alexandre.fresnais@epita.fr>
2020-03-07 18:37:37 +0100

    [TOOLS][ADD] bits operations tools

---
+++ src/tools/bits.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+size_t bits_to_depth(unsigned const char bits_count)
+{
+    return (8 * bits_count) - 1;
+}
+
+unsigned char depth_to_bits(size_t depth)
+{
+    return (depth + 1) / 8;
+}
+
+// Convert an array of size_t of a valid depth into a bytes array
+// by mapping values
+unsigned char *to_bytes(size_t original_depth, size_t *array, size_t len)
+{
+    unsigned char *new_array = malloc(sizeof(unsigned char) * len);
+
+    for (size_t i = 0; i < len; i++)
+    {
+        new_array[i] = (array[i] * 255) / original_depth;
+    }
+
+    return  new_array;
+}
--- src/tools/bits.h
+++ src/tools/bits.h
@@ -1,4 +1,10 @@
 #ifndef BITS_H
 #define BITS_H
 
+size_t bits_to_depth(unsigned const char bits_count);
+
+unsigned char depth_to_bits(size_t depth);
+
+unsigned char *to_bytes(size_t original_depth, size_t *array, size_t len)
+
 #endif /* ! BITS_H */
