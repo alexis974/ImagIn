ac6c73989909cc4a6b95cf4912adedb0cf3db448
alexis boissiere <alexis.boissiere@epita.fr>
2020-03-08 13:21:04 +0100

    [ZOOM][ADD] file structure

---
+++ src/modules/imagin/zoom.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "../../imagin.h"
+
+#include "scale.h"
+
+
+struct Image *zoom(struct Image *full_img, float zoom_value)
+{
+    //
+}
---
+++ src/modules/imagin/zoom.h
@@ -0,0 +1,5 @@
+#ifndef ZOOM_H
+#define ZOOM_H
+
+
+#endif /* ! ZOOM_H */
