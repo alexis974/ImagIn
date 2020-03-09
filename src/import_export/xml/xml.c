cd32826ffb24ebb4e19818a5025b392d75dd8882
alexis boissiere <alexis.boissiere@epita.fr>
2020-03-09 23:40:52 +0100

    [XML][ADD] more rc_handler for possible rc errors

--- src/import_export/xml/xml.c
+++ src/import_export/xml/xml.c
@@ -9,6 +9,14 @@
 #include "../../tools/history/convert.h"
 #include "xml.h"
 
+void rc_handler(int rc, char *str)
+{
+    if (rc < 0)
+    {
+        errx(1, "%s\n", str);
+    }
+}
+
 // TODO : Coding style : Fct 25 lines max
 void create_xmp(const char *uri)
 {
@@ -24,135 +32,80 @@ void create_xmp(const char *uri)
 
     /* Start the document. */
     rc = xmlTextWriterStartDocument(writer, NULL, MY_ENCODING, NULL);
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterStartDocument\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterStartDocument");
 
     /*
      ** Start an element named "IMAGE". Since thist is the first
      ** element, this will be the root element of the document.
      */
     rc = xmlTextWriterStartElement(writer, BAD_CAST "IMAGE");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterStartElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterStartElement");
 
     /* Add an attribute with name "xml:lang" and value "en" to IMAGE. */
     rc = xmlTextWriterWriteAttribute(writer, BAD_CAST "xml:lang",
             BAD_CAST "en");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterWriteAttribute\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterWriteAttribute");
 
     /* Write a comment as child of IMAGE. */
     rc = xmlTextWriterWriteFormatComment(writer, "This is a comment");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterWriteComment\n");
+    rc_handler(rc, "create_xml: Error at xmlTextWriterWriteComment");
     }
 
     /* Write a comment as child of IMAGE */
     rc = xmlTextWriterWriteFormatComment(writer, "This is another comment");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterWriteFormatComment\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterWriteFormatComment");
 
     /* Start an element named "DESCRIPTION" as child of IMAGE. */
     rc = xmlTextWriterStartElement(writer, BAD_CAST "DESCRIPTION");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterStartElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterStartElement");
 
     /* Write an element named "NAME" as child of DESCRIPTION. */
     rc = xmlTextWriterWriteElement(writer, BAD_CAST "NAME",
             BAD_CAST "This is a text.");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterWriteElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterWriteElement");
 
     /* Start an element named "EXIF" as child of DEXCRIPTION. */
     rc = xmlTextWriterStartElement(writer, BAD_CAST "EXIF");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterStartElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterStartElement");
 
     /* Write an element named "Date" as child of EXIF. */
     rc = xmlTextWriterWriteElement(writer, BAD_CAST "Date",
             BAD_CAST "This is a text.");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterWriteElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterWriteElement");
 
     /* Close the element named EXIF. */
     rc = xmlTextWriterEndElement(writer);
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterEndElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterEndElement");
 
     /* Close the element named DESCRIPTION. */
     rc = xmlTextWriterEndElement(writer);
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterEndElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterEndElement");
 
     /* Start an element named "HISTORY" as child of IMAGE. */
     rc = xmlTextWriterStartElement(writer, BAD_CAST "HISTORY");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterStartElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterStartElement");
 
     /* Write an element named "TEXT" as child of HISTORY. */
     rc = xmlTextWriterWriteElement(writer, BAD_CAST "TEXT",
             BAD_CAST "This is a text.");
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterWriteElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterWriteElement");
 
     /* Close the element named HISTORY. */
     rc = xmlTextWriterEndElement(writer);
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterEndElement\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterEndElement\n");
 
     /*
      ** Since we do not want to write any other elements, we simply call
      ** xmlTextWriterEndDocument, which will do all the work.
      */
     rc = xmlTextWriterEndDocument(writer);
-    if (rc < 0)
-    {
-        errx(1, "create_xml: Error at xmlTextWriterEndDocument\n");
-    }
+    rc_handler(rc, "create_xml: Error at xmlTextWriterEndDocument\n");
 
     xmlFreeTextWriter(writer);
-
-    //Cleanup function for the XML library.
     xmlCleanupParser();
-
-    // This is to debug memory for regression tests
     xmlMemoryDump();
 }
 
-void rc_handler(int rc, char *str)
-{
-    if (rc < 0)
-    {
-        errx(1, "%s\n", str);
-    }
-}
 
 /* Save the given history at the uri path */
 void save_hist(struct history *hist, const char *uri)
@@ -177,7 +130,6 @@ void save_hist(struct history *hist, const char *uri)
 
     rc = xmlTextWriterStartElement(writer, BAD_CAST "Modules");
     rc_handler(rc, "save_hist_xml: Error at xmlTextWriterStartElement");
-
 
     while (hist)
     {
