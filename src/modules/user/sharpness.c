6a62daa58b7442914500ad6384833f998e8e6cef
Turodoras <turodoras@gmail.com>
2020-03-12 01:15:37 +0100

    [SHARP][ADD] matrix struct

--- src/modules/user/sharpness.c
+++ src/modules/user/sharpness.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 
 #include "../../imagin.h"
 
-void sharpness(struct Image *img, float sigma)
+#include "../../tools/matrix.h"
+
+#include "sharpness.h"
+
+void blur(struct Image *img, float sigma)
+{
+    if (!img)
+    {
+        errx(1, "blur: No image found");
+    }
+
+    if (sigma <= 0)
+    {
+        return;
+    }
+
+    struct Matrix kernel;
+    initGaussianKernel(kernel, radius, sigma);
---
+++ src/tools/matrix.h
@@ -0,0 +1,11 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+struct Matrix
+{
+    size_t lines;
+    size_t cols;
+    float data[];
+};
+
+#endif
