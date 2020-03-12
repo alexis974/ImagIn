15bbf06c5734a018f5587c79224433814cb9e6fe
Turodoras <turodoras@gmail.com>
2020-03-12 18:18:35 +0100

    [SHARP][ADD] gaussian kernel init

--- src/modules/user/sharpness.c
+++ src/modules/user/sharpness.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
+#include <math.h>
 
 #include "../../imagin.h"
 
 #include "../../tools/matrix.h"
 
 #include "sharpness.h"
+
+struct Matrix initGaussianKernel(size_t radius, float sigma)
+{
+    float factor = 0;
+    struct Matrix kernel = malloc(sizeof(struct Matrix));
+    kernel.lines = radius;
+    kernel.cols = radius;
+    kernel.data = malloc(kernel.lines * kernel.cols * sizeof(float));
+
+    for (int x = -radius; x <= radius; x++)
+    {
+        for (int y = -radius; x <= radius; y++)
+        {
+            float e = exp(-(x*x + y*y) / (2*sigma*sigma));
+            factor += e;
+            kernel.data[x * cols + j];
+        }
+    }
+
+    for (int x = -radius; x <= radius; x++)
+    {
+        for (int y = -radius; x <= radius; y++)
+        {
+            kernel.data[x * cols + j] /= factor;
+        }
+    }
+
+    return kernel;
+}
 
 void blur(struct Image *img, float sigma)
 {
@@ -20,5 +50,30 @@ void blur(struct Image *img, float sigma)
         return;
     }
 
-    struct Matrix kernel;
-    initGaussianKernel(kernel, radius, sigma);
+   struct Matrix kernel = initGaussianKernel(radius, sigma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
