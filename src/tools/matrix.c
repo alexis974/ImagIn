02b52065ccf95058dfd1431b84adbd01249f5b39
Turodoras <turodoras@gmail.com>
2020-03-14 12:55:06 +0100

    [MATRIX][ADD] print fun

--- src/tools/matrix.c
+++ src/tools/matrix.c
@@ -3,8 +3,20 @@
 
 #include "matrix.h"
 
-void freeMatrix(struct Matrix matrix)
+void freeMatrix(struct Matrix m)
 {
-    free(matrix.data);
-    free(matrix);
+    free(m.data);
+    free(m);
+}
+
+void printMatrix(struct Matrix m)
+{
+    for (size_t i = 0; i < m.lines*m.cols; i++)
+    {
+        printf("%f ", m.data[i]);
+        if ((i+1) % m.cols == 0)
+        {
+            printf("\n");
+        }
+    }
 }
