1e99f48fd9114f9a55c3c51469eb2a2f4fe3237f
Turodoras <turodoras@gmail.com>
2020-03-14 15:14:10 +0100

    [SHARP][WIP] testing kernel

--- src/modules/user/sharpness.c
+++ src/modules/user/sharpness.c
@@ -9,29 +9,29 @@
 
 #include "sharpness.h"
 
-struct Matrix initGaussianKernel(size_t radius, float sigma)
+struct Matrix *initGaussianKernel(int radius, float sigma)
 {
     float factor = 0;
-    struct Matrix kernel = malloc(sizeof(struct Matrix));
-    kernel.lines = radius;
-    kernel.cols = radius;
-    kernel.data = malloc(kernel.lines * kernel.cols * sizeof(float));
+    struct Matrix *kernel = malloc(sizeof(struct Matrix));
+    kernel->lines = 2 * radius + 1;
+    kernel->cols = 2 * radius + 1;
+    kernel->data = malloc(sizeof(float) * kernel->cols * kernel->lines);
 
-    for (int x = -radius; x <= radius; x++)
+    for (int x = -radius; x < radius+1; x++)
     {
-        for (int y = -radius; x <= radius; y++)
+        for (int y = -radius; y < radius+1; y++)
         {
             float e = exp(-(x*x + y*y) / (2*sigma*sigma));
             factor += e;
-            kernel.data[x * cols + j];
+            kernel->data[x+radius * kernel->cols + y+radius] = e;
         }
     }
 
-    for (int x = -radius; x <= radius; x++)
+    for (int x = -radius; x < radius + 1; x++)
     {
-        for (int y = -radius; x <= radius; y++)
+        for (int y = -radius; y <= radius + 1; y++)
         {
-            kernel.data[x * cols + j] /= factor;
+            kernel->data[x+radius * kernel->cols + y+radius] /= factor;
         }
     }
 
@@ -50,9 +50,15 @@ void blur(struct Image *img, float sigma)
         return;
     }
 
-   struct Matrix kernel = initGaussianKernel(radius, sigma);
+    int radius = 2;
+    struct Matrix *kernel = initGaussianKernel(radius, sigma);
+    printf("\n------------------\n");
+    printf("Gaussian kernel:");
+    printf("\n------------------\n");
+    printMatrix(kernel);
 
-
+    freeMatrix(kernel);
+}
 
 
 
--- src/modules/user/sharpness.h
+++ src/modules/user/sharpness.h
@@ -1,6 +1,6 @@
 #ifndef SHARPNESS_H
 #define SHARPNESS_H
 
-//...
+void blur(struct Image *img, float sigma);
 
 #endif /* SHARPNESS_H */
