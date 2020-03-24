aec968252e4a0ad93d4aa1a57c5b965f9670259e
alexis boissiere <alexis.boissiere@epita.fr>
2020-03-24 10:55:21 +0100

    [ZOOM][WIP] add correct calculation of crop coordonates

--- src/modules/imagin/zoom.c
+++ src/modules/imagin/zoom.c
@@ -36,6 +36,20 @@ void zoom_init(struct Images *images, struct zoom2 *zoom)
     zoom->y_up = images->full->height - 1;
 }
 
+size_t get_x_pixel(struct Images *images, struct zoom2 *zoom, size_t x_center)
+{
+    float w_ratio = zoom->x_up_full / images->scale->width;
+    x_center *= w_ratio;
+    return x_center;
+}
+
+
+size_t get_y_pixel(struct Images *images, struct zoom2 *zoom, size_t y_center)
+{
+    float h_ratio = zoom->y_up_full / images->scale->height;
+    y_center *= h_ratio;
+    return y_center;
+}
 
 struct Image *zoom(struct Images *images, float *zoom_value, size_t x_center,
         size_t y_center)
@@ -57,71 +71,83 @@ struct Image *zoom(struct Images *images, float *zoom_value, size_t x_center,
     zoom_init(images, zoom);
 
     //Convert center to full image
-    float w_ratio = zoom->x_up_full / images->scale->width;
-    float h_ratio = zoom->y_up_full / images->scale->height;
-    x_center *= w_ratio;
-    y_center *= h_ratio;
+    x_center = get_x_pixel(images, zoom, x_center);
+    y_center = get_y_pixel(images, zoom, y_center);
 
+    //Get width and height of crop image
     size_t nb_x = (zoom->x_up - zoom->x_down) * 2 / 3;
     size_t nb_y = (zoom->y_up - zoom->y_down) * 2 / 3;
 
+    if ((zoom->x_up - zoom->x_down) < nb_x)
+    {
+        errx(1, "zoom: Can't crop with this width");
+    }
+    if ((zoom->y_up - zoom->y_down) < nb_y)
+    {
+        errx(1, "zoom: Can't crop with this height");
+    }
+
     printf("\npath = ");
+
 
     // X
     // Can go left no prob
-    if (x_center > (nb_x / 2))
+    if (x_center - (nb_x / 2) > zoom->x_down)
     {
         printf("1");
-        zoom->x_down -= (nb_x / 2);
+        zoom->x_down = x_center - (nb_x / 2);
+
+        //Can go right no prob
+        if (x_center + (nb_x / 2) < zoom->x_up)
+        {
+            printf("3");
+            zoom->x_up = x_center + (nb_x / 2);
+        }
+        else
+        {
+            printf("4");
+            zoom->x_up = zoom->x_up;
+            zoom->x_down = zoom->x_up - (nb_x / 2);
+        }
     }
     // Goes to far left
     else
     {
         printf("2");
-        zoom->x_down = 0;
-        zoom->x_up += (nb_x / 2) - (zoom->x_down);
+        zoom->x_down = zoom->x_down;
+        zoom->x_up = zoom->x_down + nb_x;
     }
-    // Can go right no prob
-    if (x_center + (nb_x / 2) < images->full->width - 1)
-    {
-        printf("3");
-        zoom->x_up += (nb_x / 2);
-    }
-    // Goes to far right
-    else
-    {
-        printf("4");
-        zoom->x_up = images->full->width - 1;
-        zoom->x_down -= (nb_x / 2) - ((images->full->width - 1) - x_center);
-    }
+
+
 
     // Y
     // Can go down no prob
-    if (y_center > (nb_y / 2))
+    if (y_center - (nb_y / 2) > zoom->y_down)
     {
         printf("5");
-        zoom->y_down -= (nb_y / 2);
+        zoom->y_down = y_center - (nb_y / 2);
+
+        //Can go up no prob
+        if (y_center + (nb_y / 2) < zoom->y_up)
+        {
+            printf("7");
+            zoom->y_up = y_center + (nb_y / 2);
+        }
+        else
+        {
+            printf("8");
+            zoom->y_up = zoom->y_up;
+            zoom->y_down = zoom->y_up - (nb_y / 2);
+        }
     }
-    // Goes to far to bottom
+    // Goes to far down
     else
     {
         printf("6");
-        zoom->y_down = 0;
-        zoom->y_up += (nb_y / 2) - (zoom->y_down);
+        zoom->y_down = zoom->y_down;
+        zoom->y_up = zoom->y_down + nb_y;
     }
-    // Can go up no prob
-    if (y_center + (nb_y / 2) < images->full->height - 1)
-    {
-        printf("7");
-        zoom->y_up += (nb_y / 2);
-    }
-    // Goes to far up
-    else
-    {
-        printf("8");
-        zoom->y_up = images->full->height - 1;
-        zoom->y_down -= (nb_y / 2) - ((images->full->height - 1) - y_center);
-    }
+
 
     printf("\n");
 
@@ -132,11 +158,11 @@ struct Image *zoom(struct Images *images, float *zoom_value, size_t x_center,
 
 
     // TEST :
-    if (zoom->x_up >= zoom->x_up_full)
+    if (zoom->x_up > zoom->x_up_full)
     {
         errx(1, "x_up is bigger than image width");
     }
-    if (zoom->y_up >= zoom->y_up_full)
+    if (zoom->y_up > zoom->y_up_full)
     {
         errx(1, "y_up is bigger than image height");
     }
@@ -144,3 +170,11 @@ struct Image *zoom(struct Images *images, float *zoom_value, size_t x_center,
 
     return crop(images->full, zoom->x_down, zoom->y_down, zoom->x_up, zoom->y_up);
 }
+
+/*
+struct Image *unzoom(struct Images *images, float *zoom_value, size_t x_center,
+        size_t y_center)
+{
+    //
+}
+*/
